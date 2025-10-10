253b66427e388c6d8dbf5d2ddc0502261e143c14
rene-rekowski <rene.rekowski@gmx.de>
2025-10-10 21:43:53 +0200

    add Education Component

---
+++ src/app/components/education/education.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+intface EducationItem {
+	periode: String;
+	
+}
+
+@Component({
+  selector: 'app-education',
+  imports: [],
+  templateUrl: './education.component.html',
+  styleUrl: './education.component.css'
+})
+export class EducationComponent {
+
+}
