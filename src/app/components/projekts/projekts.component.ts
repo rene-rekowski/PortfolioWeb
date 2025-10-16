8c0498a0e1c2e95bdd82df1a25f0162647904940
rene-rekowski <rene.rekowski@gmx.de>
2025-10-16 17:39:11 +0200

    add icons in projektscards

--- src/app/components/projekts/projekts.component.ts
+++ src/app/components/projekts/projekts.component.ts
@@ -14,23 +14,32 @@ interface Project {
   templateUrl: './projekts.component.html',
   styleUrls: ['./projekts.component.css']
 })
+
 export class ProjektsComponent {
 	projects: Project[] = [
-		{
-			title: 'Portfilo Website',
-			description: 'Showcase for my Projekts',
-			link: 'https://github.com/rene-rekowski/PortfolioWeb'
-		},
 		{
 			title: 'MemoryCatcher',
 			description: 'orgnaiz your Memorys',
 			link: 'https://github.com/rene-rekowski/MemoryCatcher',
+			image: '/images/projects/MemoryCatcherExample.png',
 		},
 		{
 			title: 'LetsChat',
 			description: 'Chat in with other in same Network',
 			link: 'https://github.com/rene-rekowski/LetsChat',
+			image: '/images/projects/LetsChatExample.png',
 		},
-		];
+		{
+			title: 'Portfilo Website',
+			description: 'Showcase for my Projekts',
+			link: 'https://github.com/rene-rekowski/PortfolioWeb'
+		},
+	];
+	
+	openLink(url: string): void {
+		  window.open(url, '_blank');
+		}
 
 }
+
+
