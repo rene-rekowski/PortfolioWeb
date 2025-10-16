6582eebbc2edbe660451c2c1eb3af6d158d6cb45
rene-rekowski <rene.rekowski@gmx.de>
2025-10-16 23:09:53 +0200

    add education img

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -6,6 +6,7 @@ interface EducationItem {
   title: string;
   institution: string;
   description?: string;
+  image: string;
 }
 
 @Component({
@@ -16,24 +17,28 @@ interface EducationItem {
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent {
+	 hoveredImage: string | null = null;
   education: EducationItem[] = [
     {
       period: '2022 - 2025',
       title: 'Bachelor of Science – Informatik Technischer Systeme',
       institution: 'HAW - Hochschule für angewandte Wissenschaften Hamburg',
-      description: 'Schwerpunkt auf Softwareentwicklung und eingebettete Systeme.'
+      description: 'Schwerpunkt auf Softwareentwicklung und eingebettete Systeme.',
+      image: '/images/institution/haw.png',
     },
     {
       period: '2019 - 2022',
       title: 'Staatlich anerkannter Erzieher',
       institution: 'BS30 - Fröbelseminar Hamburg',
-      description: 'Berufsbegleitende Ausbildung in der Nachmittagsbetreuung und Schulbegleitung.'
+      description: 'Berufsbegleitende Ausbildung in der Nachmittagsbetreuung und Schulbegleitung.',
+      image: '/images/institution/bs30.png',
     },
     {
 	  period: '2003 - 2027',
 	  title: 'Irena-Sendler-Schule',
 	  institution: 'Stadtteilschule',
-	  description: 'allgemeine Fachhochschulreife'
+	  description: 'allgemeine Fachhochschulreife',
+	  image: '/images/institution/irena-sendler-schule.png',
     },
   ];
 }
